f95c9660e21e1cea6216f26c7dfa0c6f6f3d557e
Joel Seng <joel29dec@gmail.com>
2019-07-07 13:42:20 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Add your functions here
+function map(src, func) {
+    let r = []
+    for (let i = 0; i < src.length; i++ ) {
+        r.push(func(src[i]));
+    }
+    return r
+  }
+
+  function reduce(src, func) {
+    for (let i = 0; i < src.length; i++ ) {
+      if (!src[i]) return false
+    }
+    return true
+  }
